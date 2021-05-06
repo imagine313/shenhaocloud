62eaef497681b6e1ed88c2ce0de52e7f894f3113
wumingkun <286393838@qq.com>
2021-05-06 20:25:37 +0800

    第一次github测试

---
+++ model/src/main/java/com/shenhao/framework/trade/BaseEntity.java
@@ -0,0 +1,8 @@
+package com.shenhao.framework.trade;
+
+/**
+ * @author: wumk
+ * @since: 2021-05-06 20:06
+ */
+public class BaseEntity {
+}
--- service/serviceuser/src/main/java/TestVO.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author: wumk
- * @since: 2021-04-21 19:58
- */
-public class TestVO {
-}
